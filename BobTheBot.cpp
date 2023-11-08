9b93af3c0194076a8dfe940ae6d704699086961d
hammad@ualberta.ca <mohammadhammad50@gmail.com>
2023-11-08 04:12:24 -0700

    Build refinery alongside barracks

--- BobTheBot.cpp
+++ BobTheBot.cpp
@@ -1,7 +1,21 @@
 #include "BobTheBot.h"
 
+
+bool isCommandCenter(const Unit& unit)
+{
+    return unit.unit_type == UNIT_TYPEID::TERRAN_COMMANDCENTER;
+}
+
+
+bool isSCV(const Unit& unit)
+{
+    return unit.unit_type == UNIT_TYPEID::TERRAN_SCV;
+}
+
+
 void BobTheBot::OnGameStart() {
 	Actions()->SendChat("Bob the Bot\nCan we fix it ?\nBob the Bot\nYes, we can!!");
+    availableSCVs = Observation()->GetUnits(Unit::Alliance::Self, isSCV);
 }
 
 
@@ -17,18 +31,19 @@ void BobTheBot::OnStep() {
 
 void BobTheBot::OnUnitIdle(const Unit* unit) {
     switch (unit->unit_type.ToType()) {
-        case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
-            const ObservationInterface* observation = Observation();
-            // If we have enough minerals and supply
-            if ((observation->GetMinerals() >= 50) && (observation->GetFoodUsed() < observation->GetFoodCap()))
-                Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
-            break;
-        }
+        // Endless SCV training
+        //case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
+        //    const ObservationInterface* observation = Observation();
+        //    // If we have enough minerals and supply
+        //    if ((observation->GetMinerals() >= 50) && (observation->GetFoodUsed() < observation->GetFoodCap()))
+        //        Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
+        //    break;
+        //}
         case UNIT_TYPEID::TERRAN_BARRACKS: {
             Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MARINE);
         }
         case UNIT_TYPEID::TERRAN_SCV: {
-            const Unit* mineral_target = FindNearestMineralPatch(unit->pos);
+            const Unit* mineral_target = FindNearest(unit->pos, UNIT_TYPEID::NEUTRAL_MINERALFIELD);
             if (!mineral_target) {
                 break;
             }
@@ -42,12 +57,71 @@ void BobTheBot::OnUnitIdle(const Unit* unit) {
 }
 
 
-const Unit* BobTheBot::FindNearestMineralPatch(const Point2D& start) {
+bool BobTheBot::TryBuildStructure(ABILITY_ID ability_type_for_structure)
+{
+    const Unit* unit_to_build = getAvailableSCV();  // CRITICAL TO-DO: Free up unit when you are done with it
+    Point2D point(unit_to_build->pos.x, unit_to_build->pos.y);
+    
+    // Edge case for refineries, you need to provide the nearest geyser
+    if (ability_type_for_structure == ABILITY_ID::BUILD_REFINERY) {
+        const Unit* nearest_geyser = FindNearest(point, UNIT_TYPEID::NEUTRAL_VESPENEGEYSER);
+        Actions()->UnitCommand(unit_to_build, ability_type_for_structure, nearest_geyser);
+    }
+
+    else {
+        // Keep finding random positions until you get a valid one for placement
+        float rx = GetRandomScalar();
+        float ry = GetRandomScalar();
+        point.x += rx * 15.0f;
+        point.y += ry * 15.0f;
+        while (!Query()->Placement(ability_type_for_structure, point))
+        {
+            rx = GetRandomScalar();
+            ry = GetRandomScalar();
+            point.x = unit_to_build->pos.x + rx * 15.0f;
+            point.y = unit_to_build->pos.y + ry * 15.0f;
+        }
+        Actions()->UnitCommand(unit_to_build, ability_type_for_structure, point);
+    }
+
+    return true;
+}
+
+
+void BobTheBot::OnBuildingConstructionComplete(const Unit* unit)
+{
+    switch (unit->unit_type.ToType()) {
+        
+        // Immediately start building more SCVS when he have space
+        case UNIT_TYPEID::TERRAN_SUPPLYDEPOT: {
+            const ObservationInterface* observation = Observation();
+            Units commandCenters = observation->GetUnits(Unit::Alliance::Self, isCommandCenter);
+            for (auto commandCenter: commandCenters) {
+                // If we have enough minerals and supply
+                if (observation->GetMinerals() >= 150 && (observation->GetFoodUsed() < observation->GetFoodCap()))
+                    if (TryBuildStructure(ABILITY_ID::BUILD_BARRACKS))
+                        numBarracks += 1;
+                if (numBarracks == 1)
+                    if (TryBuildStructure(ABILITY_ID::BUILD_REFINERY))
+                        numRefineries += 1;
+
+                    // Actions()->UnitCommand(commandCenter, ABILITY_ID::TRAIN_SCV);    // Disable infinite scv training for now
+            }
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+
+const Unit* BobTheBot::FindNearest(const Point2D& start, UNIT_TYPEID type) {
     Units units = Observation()->GetUnits(Unit::Alliance::Neutral);
     float distance = std::numeric_limits<float>::max();
     const Unit* target = nullptr;
     for (const auto& u : units) {
-        if (u->unit_type == UNIT_TYPEID::NEUTRAL_MINERALFIELD) {
+        if (u->unit_type == type) {
             float d = DistanceSquared2D(u->pos, start);
             if (d < distance) {
                 distance = d;
@@ -59,61 +133,13 @@ const Unit* BobTheBot::FindNearestMineralPatch(const Point2D& start) {
 }
 
 
-bool BobTheBot::TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type)
-{
-    const ObservationInterface* observation = Observation();
-    // If a unit already is building a supply structure of this type, do nothing.
-    const Unit* unit_to_build = nullptr;
-    Units units = observation->GetUnits(Unit::Alliance::Self);
-    for (const auto& unit : units) {
-        for (const auto& order : unit->orders) {
-            if (order.ability_id == ability_type_for_structure) {
-                return false;
-            }
-        }
-        if (unit->unit_type == unit_type) {
-            unit_to_build = unit;
-        }
-    }
-
-    // Keep finding random positions until you get a valid one for placement
-    float rx = GetRandomScalar();
-    float ry = GetRandomScalar();
-    Point2D point(unit_to_build->pos.x + rx * 15.0f, unit_to_build->pos.y + ry * 15.0f);
-    while (!Query()->Placement(ability_type_for_structure, point))
-    {
-        rx = GetRandomScalar();
-        ry = GetRandomScalar();
-        point.x = unit_to_build->pos.x + rx * 15.0f;
-        point.y = unit_to_build->pos.y + ry * 15.0f;
-    }
-
-    Actions()->UnitCommand(unit_to_build, ability_type_for_structure, point);
-    return true;
+const Unit* BobTheBot::getAvailableSCV() {
+    const Unit* available = availableSCVs.back();
+    availableSCVs.pop_back();
+    return available;
 }
 
 
-bool isCommandCenter(const Unit& unit)
-{
-    return unit.unit_type == UNIT_TYPEID::TERRAN_COMMANDCENTER;
-}
-
-
-void BobTheBot::OnBuildingConstructionComplete(const Unit* unit)
-{
-    switch (unit->unit_type.ToType()) {
-    // Immediately start building more SCVS when he have space
-        case UNIT_TYPEID::TERRAN_SUPPLYDEPOT: {
-            const ObservationInterface* observation = Observation();
-            Units commandCenters = observation->GetUnits(Unit::Alliance::Self, isCommandCenter);
-            for (auto commandCenter: commandCenters) {
-                // If we have enough minerals and supply
-                if (observation->GetMinerals() >= 50 && (observation->GetFoodUsed() < observation->GetFoodCap()))
-                    Actions()->UnitCommand(commandCenter, ABILITY_ID::TRAIN_SCV);
-            }
-            break;
-        }
-        default:
-            break;
-    }
+void BobTheBot::freeSCV(const Unit* scv) {
+    availableSCVs.push_back(scv);
 }
